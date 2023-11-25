cdc400b6472584b5f303b2d99d13a62d8fd9eda6
Daniel Chung <pikachungg@gmail.com>
2023-11-25 18:01:37 -0500

    added changes

--- app/main.ts
+++ app/main.ts
@@ -6,15 +6,17 @@ const DELIMITER = ":";
 let start = false;
 
 //Log every key that's pressed.
-v.addListener( (e, down) => {
-    KeyTranslator.keyTranslator(down)
-    console.log(
-        `${e.name} ${e.state == "DOWN" ? "DOWN" : "UP  "} [${e.rawKey._nameRaw}]`
-    );
+v.addListener((e, down) => {
+    if (e.state === 'DOWN') {
+        KeyTranslator.keyTranslator(down)
+        // console.log(
+        //     `${e.name} ${e.state == "DOWN" ? "DOWN" : "UP  "} [${e.rawKey._nameRaw}]`
+        // );
+    }
 });
 
 //Capture Windows + Space on Windows and Command + Space on Mac
-v.addListener( (e, down) => {
+v.addListener((e, down) => {
     if (e.state == "DOWN" && e.name == "SPACE" && (down["LEFT META"] || down["RIGHT META"])) {
         //call your function
         console.log("open up the safe bithces got a lot to say")
--- app/utils/KeyTranslator.ts
+++ app/utils/KeyTranslator.ts
@@ -1,4 +1,12 @@
 
+const isAlpha = (letter) => {
+    return letter.length === 1 && letter.match(/[a-z]/i);
+}
+
+const returnPressedKey = (arr) => {
+    return arr.filter( (word) => word.length === 1 )[0]
+}
+
 const filterKeys = (keys) => {
     return Object.fromEntries(
         Object.entries(keys).filter(
@@ -7,9 +15,76 @@ const filterKeys = (keys) => {
     )
 }
 
+const translateShiftNumbers = (number) => {
+    const mappings = {
+        "1": "!",
+        "2": "@",
+        "3": "#",
+        "4": "$",
+        "5": "%",
+        "6": "^",
+        "7":" &",
+        "8": "*",
+        "9": "(",
+        "0": ")"
+    }
+    return mappings[number].trim()
+}
+
+const translateKey = (keys) => {
+    let allKeys = Object.getOwnPropertyNames(keys)
+
+    let isShift = allKeys.includes('RIGHT SHIFT') || allKeys.includes('LEFT SHIFT')
+    let isCaplock = allKeys.includes('CAPS_LOCK')
+    let isEnter = allKeys.includes('RETURN')
+    let isBackSpace = allKeys.includes('BACKSPACE')
+    let isBackSlash = allKeys.includes('BACKSLASH')
+    let isForwardSlash = allKeys.includes('FORWARD SLASH')
+    let isDot = allKeys.includes('DOT')
+    let isComma = allKeys.includes('COMMA')
+    let isUpArrow = allKeys.includes('UP ARROW')
+    let isLeftArrow = allKeys.includes('LEFT ARROW')
+    let isRightArrow = allKeys.includes('RIGHT ARROW')
+    let isDownArrow = allKeys.includes('DOWN ARROW')
+    let isMinus = allKeys.includes('MINUS')
+    let isEquals = allKeys.includes('EQUALS')
+    let isMeta = allKeys.includes('RIGHT META') || allKeys.includes('LEFT META')
+    let isAlt = allKeys.includes('RIGHT ALT') || allKeys.includes('LEFT ALT')
+    let isCtrl = allKeys.includes('LEFT CTRL') || allKeys.includes('RIGHT CTRL')
+    let isTab = allKeys.includes('TAB')
+    let isBacktick = allKeys.includes('BACKTICK')
+    let isEsc = allKeys.includes('ESCAPE')
+    let isSquareBracketOpen = allKeys.includes('SQUARE BRACKET CLOSE') //[
+    let isSquareBracketClose = allKeys.includes('SQUARE BRACKET OPEN') //]
+    let isQuote = allKeys.includes('QUOTE')
+    let isSpace = allKeys.includes('SPACE')
+
+    let pressedKey = returnPressedKey(allKeys)
+
+    //If it's just shift + letters
+    if (isShift && pressedKey && isAlpha(pressedKey)){
+        console.log(pressedKey.toUpperCase())
+    }
+    //If it's just normal letters
+    if (!isShift && pressedKey && isAlpha(pressedKey)){
+        console.log(pressedKey.toLowerCase())
+    }
+    //If it's shit + a number
+    if (isShift && pressedKey && !isAlpha(pressedKey)){
+        console.log(translateShiftNumbers(pressedKey))
+    }
+    
+
+    // console.log(pressedKey)
+
+    // console.log(isAlpha(pressedKey))
+    // console.log(pressedKey + "hereeee")
+
+}
+
 const keyTranslator = (keys) => {
     let currentKeys = filterKeys(keys)
-    console.log(currentKeys)
+    translateKey(currentKeys)
 }
 
 module.exports = {
