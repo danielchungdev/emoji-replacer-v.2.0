58d314d4ee36cea33ccd80dd3dfdc6c9cbea1ccb
Daniel Chung <pikachungg@gmail.com>
2024-01-11 18:22:50 -0500

    commiting changes between pc restart

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,9 +2,11 @@ import "./Navbar.css"
 import { Cog8ToothIcon } from "@heroicons/react/24/solid"
 
 export const Navbar = () => {
-    return(
+    return (
         <div className="navbar">
-            <Cog8ToothIcon className="navbar-option"/>
+            <div className="options-outer-bar">
+                <Cog8ToothIcon className="navbar-option" />
+            </div>
         </div>
     )
 }
